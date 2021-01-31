4f882f088712d9314c8655c5fa4a621e13a01b20
Pavel Rochnyack <pavel2000@ngs.ru>
2021-01-31 06:04:56 +0000

    rrdtool plugin: use rrd_updatex_r with RRD_SKIP_PAST_UPDATES flag
    This required to avoid data loss when timestamp not grow for any reasons,
    (for example - duplicated or reordered UDP packet, or due to possible bug in
    Collectd core) and buggy Collectd code was unable to check this properly.
    
    This resolves cases which are logged with a message "illegal attempt to update
    using time XXX when last update time is XXX (minimum one second step)".
    Recent rrdtool (>=1.5.0) allows to skip only certain datapoints, not all data.
    
    collectd/collectd#1058

--- src/rrdtool.c
+++ src/rrdtool.c
@@ -109,7 +109,12 @@ static int srrd_update(char *filename, char *template, int argc,
   optind = 0; /* bug in librrd? */
   rrd_clear_error();
 
+#ifdef RRD_SKIP_PAST_UPDATES
+  int status = rrd_updatex_r(filename, template, RRD_SKIP_PAST_UPDATES, argc, (void *)argv);
+#else
   int status = rrd_update_r(filename, template, argc, (void *)argv);
+#endif
+
   if (status != 0) {
     WARNING("rrdtool plugin: rrd_update_r (%s) failed: %s", filename,
             rrd_get_error());
